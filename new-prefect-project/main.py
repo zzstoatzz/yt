475806f7dd62e93aa060aa33660e0e5b4d37214b
zzstoatzz <thrast36@gmail.com>
2024-08-12 21:38:54 -0500

    demo

---
+++ new-prefect-project/main.py
@@ -0,0 +1,39 @@
+from datetime import timedelta
+
+from prefect import flow, task
+from prefect.client.schemas.schedules import IntervalSchedule
+
+
+@task(retries=3)
+def load_data(query: str) -> list[dict]:
+    """Load data from a database or API."""
+    return [{"name": "Alice", "age": 30}, {"name": "Bob", "age": 25}]
+
+
+@task
+def process_data(data: list[dict]) -> list[dict]:
+    """Process the data."""
+    return [{"name": d["name"], "age": d["age"] + 1} for d in data]
+
+
+@task
+def save_data(data: list[dict]) -> None:
+    """Save the processed data to a database or file."""
+    print(data)
+
+
+@flow
+def main(query: str = "query"):
+    data = load_data(query)
+    print(f"loaded data: {data}")
+    processed_data = process_data(data)
+    print(f"processed data: {processed_data}")
+    save_data(processed_data)
+    print(f"saved data: {processed_data}")
+
+
+if __name__ == "__main__":
+    main.serve(
+        name="My First Deployment",
+        schedules=[IntervalSchedule(interval=timedelta(minutes=1))],
+    )
--- src/yt/_version.py
+++ src/yt/_version.py
@@ -13,5 +13,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = "0.1.dev18+g253cdaf.d20240617"
-__version_tuple__ = version_tuple = (0, 1, "dev18", "g253cdaf.d20240617")
+__version__ = version = "0.1.dev21+g839769b.d20240813"
+__version_tuple__ = version_tuple = (0, 1, "dev21", "g839769b.d20240813")
